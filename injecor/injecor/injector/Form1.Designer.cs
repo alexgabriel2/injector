da9cda0aab3090f9d51a90f425a7fecf699db896
alexgabriel2 <49072620+alexgabriel2@users.noreply.github.com>
2020-04-08 13:20:34 +0300

    Add files via upload

---
+++ injecor/injecor/injector/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.memepanel2 = new injector.memepanel();
+            this.memepanel1 = new injector.memepanel();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(317, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 103);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Inject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(317, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 52);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(317, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 52);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Pick .DLL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(12, 57);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(299, 115);
+            this.panel3.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(14, 89);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 16);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Current Time: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(15, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 16);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Current date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(14, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Steam Working:";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(14, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "DLL Name:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Location = new System.Drawing.Point(12, 178);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(299, 53);
+            this.panel4.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(15, 23);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(155, 16);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Discord: green line#0740";
+            this.label10.Click += new System.EventHandler(this.Label10_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(14, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 16);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Contact";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(299, 41);
+            this.panel1.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Un injector frumos facut de green line";
+            // 
+            // memepanel2
+            // 
+            this.memepanel2.angle = 226F;
+            this.memepanel2.borderRadius = 20;
+            this.memepanel2.color0 = System.Drawing.Color.Green;
+            this.memepanel2.color1 = System.Drawing.Color.Teal;
+            this.memepanel2.Location = new System.Drawing.Point(-102, 237);
+            this.memepanel2.Name = "memepanel2";
+            this.memepanel2.Size = new System.Drawing.Size(821, 4);
+            this.memepanel2.TabIndex = 17;
+            // 
+            // memepanel1
+            // 
+            this.memepanel1.angle = 233F;
+            this.memepanel1.borderRadius = 20;
+            this.memepanel1.color0 = System.Drawing.Color.Green;
+            this.memepanel1.color1 = System.Drawing.Color.Teal;
+            this.memepanel1.Location = new System.Drawing.Point(-3, -19);
+            this.memepanel1.Name = "memepanel1";
+            this.memepanel1.Size = new System.Drawing.Size(821, 23);
+            this.memepanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(504, 240);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.memepanel2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.memepanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private memepanel memepanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private memepanel memepanel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ injecor/injecor/injector/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Management;
+using System.IO;
+using System.Net;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Net.Sockets;
+using System.Web;
+namespace injector
+
+{//inject
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateRemoteThread(
+         IntPtr hProcess,
+         IntPtr lpThreadAttributes,
+         uint dwStackSize,
+         UIntPtr lpStartAddress, // raw Pointer into remote process
+         IntPtr lpParameter,
+         uint dwCreationFlags,
+         out IntPtr lpThreadId
+       );
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(
+            UInt32 dwDesiredAccess,
+            Int32 bInheritHandle,
+            Int32 dwProcessId
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(
+        IntPtr hObject
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern bool VirtualFreeEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            UIntPtr dwSize,
+            uint dwFreeType
+            );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true)]
+        public static extern UIntPtr GetProcAddress(
+            IntPtr hModule,
+            string procName
+            );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            uint dwSize,
+            uint flAllocationType,
+            uint flProtect
+            );
+
+        [DllImport("kernel32.dll")]
+        static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            string lpBuffer,
+            UIntPtr nSize,
+            out IntPtr lpNumberOfBytesWritten
+        );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(
+            string lpModuleName
+            );
+
+        [DllImport("kernel32", SetLastError = true, ExactSpelling = true)]
+        internal static extern Int32 WaitForSingleObject(
+            IntPtr handle,
+            Int32 milliseconds
+            );
+
+        public Int32 GetProcessId(String proc) {
+            Process[] ProcList;
+            ProcList = Process.GetProcessesByName(proc);
+            return ProcList[0].Id;
+        }
+
+
+        public void InjectDLL(IntPtr hProcess, String strDLLName){
+            IntPtr bytesout;
+
+            Int32 LenWrite = strDLLName.Length + 1;
+            IntPtr AllocMem = (IntPtr)VirtualAllocEx(hProcess, (IntPtr)null, (uint)LenWrite, 0x1000, 0x40);
+
+            WriteProcessMemory(hProcess, AllocMem, strDLLName, (UIntPtr)LenWrite, out bytesout);
+            UIntPtr Injector = (UIntPtr)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            if (Injector == null){
+                MessageBox.Show(" Injector Error! \n ");
+                return;
+            }
+            IntPtr hThread = (IntPtr)CreateRemoteThread(hProcess, (IntPtr)null, 0, Injector, AllocMem, 0, out bytesout);
+            if (hThread == null){
+                MessageBox.Show(" hThread [ 1 ] Error! \n ");
+                return;
+            }
+           // int Result = WaitForSingleObject(hThread, 10 * 1000);
+            //if (Result == 0x00000080L || Result == 0x00000102L || Result == 0xFFFFFFFF) {
+            //    MessageBox.Show(" hThread [ 2 ] Error! \n ");
+            //    if (hThread != null){
+            //        CloseHandle(hThread);
+            //    }
+            //    return;
+            //}
+            Thread.Sleep(1000);
+            VirtualFreeEx(hProcess, AllocMem, (UIntPtr)0, 0x8000);
+            if (hThread != null){
+                CloseHandle(hThread);
+            }
+            return;
+        }
+        private void Form1_Load(object sender, EventArgs e){
+        }
+        private void Button3_Click(object sender, EventArgs e){
+            openFileDialog1.Filter = "DLL Files (*.dll)|*.dll|All files (*.*)|*.*";//dll pick
+            string filename;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK){
+                filename = openFileDialog1.FileName;
+                string result;
+                result = Path.GetFileNameWithoutExtension(filename);
+                label5.Text = "DLL Name: " + result;
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e){
+            try{
+                string pprocess = "csgo";
+                if (Process.GetProcessesByName(pprocess).Length == 1){
+                    try {
+                        Int32 process = GetProcessId(pprocess);
+                        if(process >= 0) {
+                            IntPtr hProcess = (IntPtr)OpenProcess(0x1F0FFF, 1, process);                     
+                            try {
+                                if(process == 0){
+                                    var button = MessageBoxButtons.OK;
+                                    var icon = MessageBoxIcon.Error;
+                                    string text = "Injector by green line!";
+                                    MessageBox.Show("Injection failed", text, button, icon);
+                                    return;
+                                }
+                                else{
+                                    string filename = openFileDialog1.FileName;
+                                    var button = MessageBoxButtons.OK;
+                                    var icon = MessageBoxIcon.Information;
+                                    string text = "Injector by green line!";
+                                    InjectDLL(hProcess, filename);
+                                    MessageBox.Show("DLL has been injected!", text, button, icon);
+                                }
+                            }
+                            catch(Exception ex){
+                                var button = MessageBoxButtons.OK;
+                                var icon = MessageBoxIcon.Error;
+                                string text = "Injector by green line!";
+                                MessageBox.Show("Injection Error: " + ex, text, button, icon);
+                            }
+                        }
+                    }
+                    catch(Exception ex){
+                        var button = MessageBoxButtons.OK;
+                        var icon = MessageBoxIcon.Error;
+                        string text = "Injector by green line!";
+                        MessageBox.Show("Injection Error: " + ex, text, button, icon);
+                    }
+                }
+                else{
+                    var button = MessageBoxButtons.OK;
+                    var icon = MessageBoxIcon.Error;
+                    string text = "Injector by green line!";
+                    MessageBox.Show("Error: Please start CS:GO First", text, button, icon);
+                    return;
+                }
+            }
+            catch(Exception ex){
+                var button = MessageBoxButtons.OK;
+                var icon = MessageBoxIcon.Error;
+                string text = "Injector by green line!";
+                MessageBox.Show("Injection Error: " + ex, text, button, icon);
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e){
+            Application.Exit();
+        }
+        private void Timer1_Tick(object sender, EventArgs e) {
+            string csgo = "csgo";
+            switch(Process.GetProcessesByName(csgo).Length){//csgo status
+                case 1:
+                label6.Text = "CS:GO Working: true";
+                break;
+
+            default:
+                label6.Text = "CS:GO Working: false";
+                break;
+            }
+            label7.Text = "Current Date: " + DateTime.Now.ToLongDateString();//Current Date
+            label8.Text = "Current Time: " + DateTime.Now.ToLongTimeString();//Current Time
+        }
+
+        private void Label10_Click(object sender, EventArgs e){
+        }
+
+        private void Label6_Click(object sender, EventArgs e){
+        }
+    }
+}
